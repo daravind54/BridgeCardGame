eb8f2881a01df184bd32482add4f5904d75f5156
darav <darav@10.28.128.72>
2016-11-23 14:55:42 -0500

    File /C:/Aravind/TempWorkSpace/BridgeCardGame/src/com/gmu/edu/RegistrationDao.java changed at Wed Nov 23 14:55:42 EST 2016

--- src/com/gmu/edu/RegistrationDao.java
+++ src/com/gmu/edu/RegistrationDao.java
@@ -2,8 +2,9 @@ package com.gmu.edu;
 
 public class RegistrationDao 
 {
-	public void registerUser(Registration registration)
+	public void registerUser(Registration registration) throws ClassNotFoundException
 	{
-		
+		Class.forName("oracle.jdbc.driver.OracleDriver"); 
+		Connection con=DriverManager.getConnection("jdbc:oracle:thin:@apollo.vse.gmu.edu:1521:ite10g","adasari2","psaduh");
 	}
 }
