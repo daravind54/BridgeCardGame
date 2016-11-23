acee780404721438cd1cce84b47bcdbc9a99ff3f
darav <darav@10.28.128.72>
2016-11-23 15:25:56 -0500

    File /C:/Aravind/TempWorkSpace/BridgeCardGame/src/com/gmu/edu/LoginDao.java changed at Wed Nov 23 15:25:56 EST 2016

--- src/com/gmu/edu/LoginDao.java
+++ src/com/gmu/edu/LoginDao.java
@@ -11,5 +11,7 @@ public class LoginDao
 		Class.forName("oracle.jdbc.driver.OracleDriver"); 
 		Connection con=DriverManager.getConnection("jdbc:oracle:thin:@apollo.vse.gmu.edu:1521:ite10g","adasari2","eecooc");
 		String sql="select * from userdetails";
+		
+		return sql;
 	}
 }
