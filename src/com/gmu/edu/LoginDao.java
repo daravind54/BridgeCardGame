65d526f8a5270e03ab34d4e5761fbe464e9fb4dd
darav <darav@10.28.128.72>
2016-11-23 15:30:44 -0500

    File /C:/Aravind/TempWorkSpace/BridgeCardGame/src/com/gmu/edu/LoginDao.java changed at Wed Nov 23 15:30:44 EST 2016

--- src/com/gmu/edu/LoginDao.java
+++ src/com/gmu/edu/LoginDao.java
@@ -2,7 +2,9 @@ package com.gmu.edu;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class LoginDao 
 {
@@ -10,8 +12,14 @@ public class LoginDao
 	{
 		Class.forName("oracle.jdbc.driver.OracleDriver"); 
 		Connection con=DriverManager.getConnection("jdbc:oracle:thin:@apollo.vse.gmu.edu:1521:ite10g","adasari2","eecooc");
-		String sql="select * from userdetails";
-		
+		String sql="select email,password from userdetails";
+		Statement statement=con.createStatement();
+		ResultSet resultSet=statement.executeQuery(sql);
+		while(resultSet.next())
+		{
+			String email=resultSet.getString("email");
+			String password=resultSet.getString("password");
+		}
 		return sql;
 	}
 }
