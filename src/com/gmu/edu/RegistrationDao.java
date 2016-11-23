db681291eae1794af2695426c0a718192eaf3f84
darav <darav@10.28.128.72>
2016-11-23 14:57:17 -0500

    File /C:/Aravind/TempWorkSpace/BridgeCardGame/src/com/gmu/edu/RegistrationDao.java changed at Wed Nov 23 14:57:17 EST 2016

--- src/com/gmu/edu/RegistrationDao.java
+++ src/com/gmu/edu/RegistrationDao.java
@@ -1,10 +1,15 @@
 package com.gmu.edu;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class RegistrationDao 
 {
-	public void registerUser(Registration registration) throws ClassNotFoundException
+	public void registerUser(Registration registration) throws ClassNotFoundException, SQLException
 	{
 		Class.forName("oracle.jdbc.driver.OracleDriver"); 
-		Connection con=DriverManager.getConnection("jdbc:oracle:thin:@apollo.vse.gmu.edu:1521:ite10g","adasari2","psaduh");
+		Connection con=DriverManager.getConnection("jdbc:oracle:thin:@apollo.vse.gmu.edu:1521:ite10g","adasari2","eecooc");
+		String sql="insert into STUDENT VALUES (?, ?, ?, ?)";
 	}
 }
