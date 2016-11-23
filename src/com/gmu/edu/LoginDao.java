4056cf219602ef8f9302e50da9622b0b64ce4f38
darav <darav@10.28.128.72>
2016-11-23 15:25:50 -0500

    File /C:/Aravind/TempWorkSpace/BridgeCardGame/src/com/gmu/edu/LoginDao.java changed at Wed Nov 23 15:25:50 EST 2016

--- src/com/gmu/edu/LoginDao.java
+++ src/com/gmu/edu/LoginDao.java
@@ -6,9 +6,10 @@ import java.sql.SQLException;
 
 public class LoginDao 
 {
-	public void loginUser(Login login) throws ClassNotFoundException, SQLException
+	public String loginUser(Login login) throws ClassNotFoundException, SQLException
 	{
 		Class.forName("oracle.jdbc.driver.OracleDriver"); 
 		Connection con=DriverManager.getConnection("jdbc:oracle:thin:@apollo.vse.gmu.edu:1521:ite10g","adasari2","eecooc");
+		String sql="select * from userdetails";
 	}
 }
