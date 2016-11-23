e19e088ba9a5097bb37f199c7b86cbe7e9e7987c
darav <darav@10.28.128.72>
2016-11-23 15:01:07 -0500

    File /C:/Aravind/TempWorkSpace/BridgeCardGame/src/com/gmu/edu/RegistrationDao.java changed at Wed Nov 23 15:01:07 EST 2016

--- src/com/gmu/edu/RegistrationDao.java
+++ src/com/gmu/edu/RegistrationDao.java
@@ -2,6 +2,7 @@ package com.gmu.edu;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class RegistrationDao 
@@ -11,5 +12,11 @@ public class RegistrationDao
 		Class.forName("oracle.jdbc.driver.OracleDriver"); 
 		Connection con=DriverManager.getConnection("jdbc:oracle:thin:@apollo.vse.gmu.edu:1521:ite10g","adasari2","eecooc");
 		String sql="insert into STUDENT VALUES (?, ?, ?, ?)";
+		PreparedStatement preparedStatement=con.prepareStatement(sql);
+		preparedStatement.setString(1, registration.getName());
+		preparedStatement.setString(2, registration.getCity());
+		preparedStatement.setString(3, registration.getEmail());
+		preparedStatement.setString(4, registration.getPassword());
+		preparedStatement.executeUpdate();
 	}
 }
