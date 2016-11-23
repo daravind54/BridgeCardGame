3d0ffdc2ba5b5207c6636093709087e7e7604383
darav <darav@10.28.128.72>
2016-11-23 15:23:17 -0500

    File /C:/Aravind/TempWorkSpace/BridgeCardGame/src/com/gmu/edu/Login.java changed at Wed Nov 23 15:23:17 EST 2016

--- src/com/gmu/edu/Login.java
+++ src/com/gmu/edu/Login.java
@@ -3,5 +3,17 @@ package com.gmu.edu;
 public class Login {
 	private String email;
 	private String password;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
