53b7da4f48945556e2039e9f9d359f8367fbc12b
darav <darav@10.28.128.87>
2016-11-25 15:01:38 -0500

    File /C:/Aravind/TempWorkSpace/BridgeCardGame/src/com/gmu/sockets/WebSocketSessionHandler.java changed at Fri Nov 25 15:01:38 EST 2016

--- WebContent/game.jsp
+++ WebContent/game.jsp
@@ -5,8 +5,9 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 <title>Insert title here</title>
+<script src="temp.js"></script>
 </head>
 <body>
-todo
+
 </body>
 </html>
--- WebContent/temp1.js
+++ WebContent/temp1.js
@@ -3,8 +3,8 @@ var socket = new WebSocket("wss://localhost/BridgeCardGame/actions");
 socket.onmessage = onMessage;
 function onMessage(event) {
 	
-	//alert("On message");
-	window.location.href = "game.jsp";
+	alert("event");
+	//window.location.href = "game.jsp";
 }
 
 function formSubmit() {
