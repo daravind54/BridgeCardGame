10d6e7defb53309979b141451215bc2f02053bd8
darav <darav@DESKTOP-GCKRKSV.fios-router.home>
2016-11-26 16:45:07 -0500

    File /C:/Aravind/TempWorkSpace/BridgeCardGame/src/com/gmu/bridge/PlayerCard.java changed at Sat Nov 26 16:45:07 EST 2016

--- src/com/gmu/bridge/PlayerCard.java
+++ src/com/gmu/bridge/PlayerCard.java
@@ -5,5 +5,16 @@ import javax.json.JsonObject;
 
 public class PlayerCard 
 {
-	public JsonObject assignCardsToPlayer() 
+	public JsonObject assignCardsToPlayer(JsonObject toClient) 
+	{
+		for (int i = 0; i <= 12; i++) {
+			Card card=deck.get(i);
+			cardString=card.getSuit()+"-"+card.getRank();
+			cardname="card"+(i+1);
+			
+			cardDetails = provider.createObjectBuilder()
+					.add(cardname, cardString).build();
+			clientMessage.putAll(cardDetails);
+		}
+	}
 }
