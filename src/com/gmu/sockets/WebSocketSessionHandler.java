1cdc023369e6ea04630cbf0ebbe40ae2ef1f867c
darav <darav@DESKTOP-GCKRKSV.byod.gmu.edu>
2016-12-06 18:31:42 -0500

    File /C:/Aravind/TempWorkSpace/BridgeCardGame/src/com/gmu/sockets/WebSocketSessionHandler.java changed at Tue Dec 06 18:31:42 EST 2016

--- src/com/gmu/sockets/WebSocketSessionHandler.java
+++ src/com/gmu/sockets/WebSocketSessionHandler.java
@@ -207,7 +207,7 @@ public class WebSocketSessionHandler
 		 String temp=null;
 		 while(is.hasNext())
 		 {
-			 String key=is.next()
+			 String key=is.next();
 			 if(lastElement==cardRank.get(key))
 			 {
 				 temp=key;
