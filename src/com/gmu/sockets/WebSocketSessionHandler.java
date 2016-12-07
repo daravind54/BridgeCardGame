107517789c037e1cf9e813cf23c7f029afda4632
darav <darav@DESKTOP-GCKRKSV.byod.gmu.edu>
2016-12-06 22:43:06 -0500

    File /C:/Aravind/TempWorkSpace/BridgeCardGame/src/com/gmu/sockets/WebSocketSessionHandler.java changed at Tue Dec 06 22:43:06 EST 2016

--- src/com/gmu/sockets/WebSocketSessionHandler.java
+++ src/com/gmu/sockets/WebSocketSessionHandler.java
@@ -409,7 +409,9 @@ public class WebSocketSessionHandler
 		{
 			String temp=it.next();
 			System.out.println(temp);
+			System.out.println(jsonMessage.getString(temp.toString());
 			String temp1=jsonMessage.getString(temp.toString());
+			System.out.println(temp1);
 			if(!(temp.equals("card1")))
 			{
 				data1=provider1.createObjectBuilder()
